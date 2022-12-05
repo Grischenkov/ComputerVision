543cf956cdfe097c9b3b082feaf2d50314e0108f
Nikita Grishchenkov <nikita.grishenkov@gmail.com>
2022-12-05 17:08:33 +0300

    feat(lab1): добавить решение с использованием Numba

---
+++ Lab1/Solution/NumbaSolution.py
@@ -0,0 +1,34 @@
+import time
+import numpy as np
+
+from numba import njit
+
+from Solution.Solution import Solution
+
+class NumbaSolution(Solution): 
+    def __init__(self):
+        super().__init__()
+    def dilation(self, kernel):
+        start_time = time.time()
+        self.result_image = solution(self.source_image, kernel)
+        self.time = time.time() - start_time
+    def binarization(image):
+        print("Метод не реализован!")
+
+@njit
+def solution(source_image, kernel):
+    y = kernel.shape[0] // 2
+    x = kernel.shape[1] // 2
+    processed_image = np.copy(source_image)
+    for chanel in range (source_image.shape[2]):
+        for i in range(y, source_image.shape[0] - y):
+            for j in range(x, source_image.shape[1] - x):
+                local_window = source_image[i-y:i+y+1, j-x:j+x+1, chanel]
+                max = 0
+                for z in range(local_window.shape[0]):
+                    for c in range(local_window.shape[1]):
+                        if kernel[z][c] == 1:
+                            if local_window[z][c] > max:
+                                max = local_window[z][c]
+                processed_image[i][j][chanel] = max
+    return processed_image
