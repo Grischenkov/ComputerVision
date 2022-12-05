bac67795bd281463bc46273d53c4ed9616ef715c
Nikita Grishchenkov <nikita.grishenkov@gmail.com>
2022-12-05 13:42:12 +0300

    fix(lab1): Исправить базовый класс решения

--- Lab1/Solution/Solution.py
+++ Lab1/Solution/Solution.py
@@ -3,28 +3,61 @@ import cv2
 
 class Solution():
     def __init__(self):
-        self.time = 0
-        self.path = ''
-        self.result_image = None
-        self.source_image = self._load_image()
-    def _load_image(self):
+        self.__time = 0
+        self.__path = ''
+        self.__source_image = None
+        self.__result_image = None
+
+    @property
+    def time(self):
+        return self.__time
+    @time.setter
+    def time(self, time):
+        self.__time = time
+    
+    @property
+    def path(self):
+        return self.__path
+    @path.setter
+    def path(self, path):
+        self.__path = path
+
+    @property
+    def source_image(self):
+        return self.__source_image
+    @source_image.setter
+    def source_image(self, image):
+        self.__source_image = image
+
+    @property
+    def result_image(self):
+        return self.__result_image
+    @result_image.setter
+    def result_image(self, image):
+        self.__result_image = image
+
+    def load_source_image(self):
         while True:
             try:
-                file_path = input(f"Путь к файлу: ")
-                self.source_image = cv2.imread(file_path)
-                self.path = file_path
+                self.path = input(f"Путь к файлу: ")
+                self.source_image = cv2.imread(self.path)
                 return
             except:
                 print(f"Некорректный путь!")
-    def show_source(self):
+
+    def show_source_image(self):
         cv2.imshow(f"Source image", self.source_image)
         cv2.waitKey(0)
-    def show_result(self, algorithm, realisation):
+
+    def show_result_image(self, algorithm, realisation):
         cv2.imshow(f"Result image ({algorithm}, {realisation})", self.result_image)
         cv2.waitKey(0)
-    def save_result(self, algorithm, realisation):
+
+    def save_result_image(self, algorithm, realisation):
         cv2.imwrite(f"{os.path.splitext(self.path)[0]}_{algorithm}_{realisation}{os.path.splitext(self.path)[1]}", self.result_image)
+
     def dilatation(self, kernel):
         pass
+
     def binarization(self):
         pass
